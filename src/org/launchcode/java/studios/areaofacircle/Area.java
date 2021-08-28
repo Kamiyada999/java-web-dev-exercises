4d758c6257cbc7774fc5b1ffe5328e8b0b77ba3a
Kamiyada <you@example.comSonicSniper284@hotmail.com>
2021-08-28 02:52:20 -0500

    studio

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,17 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input;
+        public static Double getArea(Double radius) {
+            return 3.14 * radius * radius;
+        }
+        input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        String name = input.next();
+        input.close();
+
+
+    }
+
+}
